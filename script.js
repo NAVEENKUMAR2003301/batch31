fd4d25db0617d8697ea9275cad514e1d3d0febd1
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-04-15 17:32:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,210 @@
+// variable
+
+// // container - we can store the data / value
+
+// // three type - var / let / const
+
+// // rules of variable
+
+// // var  - global scope
+
+// // 1. declration , 2. intialization
+
+// var a = 10;
+
+// // 3. reuse , 4. reInialization
+
+// a = 20;
+
+// // 5. reDeclration
+
+// var a = 30;
+
+// console.log(a);
+
+// // let - block scope
+
+// // declration  // intialization
+
+// let b = 10;
+
+// // reuse      // reIntialization
+
+// b = 60;
+
+// // reDeclration not accept
+
+// // let b = 50
+
+// console.log(b);
+
+// // const - block scope
+
+// // declration     // intialiazation
+
+// const c = 30;
+
+// // reuse   // reIntialiazation not accept
+
+// //   c = 40
+
+// // reDeclration not accept
+
+// // const c = 50
+
+// console.log(c);
+
+// example :-
+
+var a = 10;
+a = 20;
+var a = 30;
+console.log(a);   //  30
+
+let b = 15;
+b = 25;
+console.log(b);   //  25
+
+
+const c = 50;
+console.log(c);    // 50
+
+
+var x = 5;
+x = x + 10;
+console.log(x);  // 15
+
+
+let y = 40;
+y = y - 10;
+console.log(y);   // 30
+
+
+
+const z = 20;
+const result = z + 5;
+console.log(result); // 25
+
+
+var m = 1;
+var m = 2;
+m = 5;
+console.log(m);  // 5
+
+
+let n = 100;
+n = n / 2;
+console.log(n);   // 50
+
+
+const p = 10;
+const q = p * 3;
+console.log(q);   // 30
+
+
+var r = 2;
+r = r * 3;
+var r = r + 4;
+console.log(r);   // 10
+
+
+
+// Printing Statement
+
+
+// 1. console.log()
+
+console.log(100);
+
+let a1 = 10
+
+console.log(a1);
+
+
+// 2. alert()
+
+
+// alert(100)
+
+// let b1 = 120
+
+// alert(b1)
+
+
+// 3. confirm()
+
+// confirm("did you like js?")
+
+
+// 4. prompt()
+
+// prompt("what is your name :-")
+
+
+// 5. document.writeln()
+
+// document.writeln(120)
+
+
+
+// i want to print value in console side. "welcome"
+
+
+console.log("welcome");
+
+
+
+
+// i want to print the value to pop up  . "Good evening"
+
+// alert("Good evening")
+
+// i want to know the wish to user final val in console("do you like apple")
+
+
+// let userWish = confirm("do you like apple ?")
+
+// console.log(userWish);
+
+
+
+
+
+
+// i want to print in console side user age .
+
+// let userAge = prompt("what is your age :- ")
+
+// console.log("user age :- " + userAge);
+
+
+
+
+// console method :- 
+
+
+// console.log()
+
+console.log(100);
+
+
+
+// console.warn()
+
+console.warn(100);
+
+
+
+// console.error()
+
+console.error(100)
+
+
+// console.clear()
+
+console.clear()
+
+
+console.log("good");
+console.error("bad");
+
